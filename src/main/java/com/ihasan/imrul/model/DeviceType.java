ef4fe3f6a3ebb8c2a2584b91c75e016f1865bb41
imrul001 <imrul001@gmail.com>
2023-10-08 11:36:57 +0600

    add a method to get Device Name from description

--- src/main/java/com/ihasan/imrul/model/DeviceType.java
+++ src/main/java/com/ihasan/imrul/model/DeviceType.java
@@ -14,4 +14,21 @@ public enum DeviceType {
     public String getDescription() {
         return description;
     }
+    
+    // not being used 
+    public String getDeviceName(String description){
+        for(DeviceType device : DeviceType.values()){
+            if (DeviceType.DESKTOP.getDescription().equals(device.getDescription())){
+                return DeviceType.DESKTOP.toString();
+            }
+            if (DeviceType.TABLET.getDescription().equals(device.getDescription())){
+                return DeviceType.TABLET.toString();
+            }
+            if (DeviceType.MOBILE.getDescription().equals(device.getDescription())){
+                return DeviceType.MOBILE.toString();
+            }
+            return null;
+        }
+        return null;
+    }
 }
